cf572bda53e57fea0f9828173389f98e003bf7b7
Bhola-B2C <bholajrs@gmail.com>
2017-12-23 20:17:52 +0530

    Auto refresh added

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var L = require('leaflet');
 var omnivore = require('leaflet-omnivore');
-var ip = require('ip');
-var add = ip.address();
 
 var mymap = L.map('map').setView([23.552488, 87.293613],15);
+var f = require('fs');
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    	maxZoom: 18,
-    	id: 'mapbox.streets',
-    	accessToken: 'pk.eyJ1IjoiYmhvbGEtYjJjIiwiYSI6ImNqYmdicWlmMDFnM2IycnF3bm9meDlieDIifQ.6edZqKmL3_kl0crsw-UQvw'
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoiYmhvbGEtYjJjIiwiYSI6ImNqYmdicWlmMDFnM2IycnF3bm9meDlieDIifQ.6edZqKmL3_kl0crsw-UQvw'
 }).addTo(mymap);
 
 var getJSON = function(url, callback) {
@@ -26,17 +24,71 @@ var getJSON = function(url, callback) {
     };
     xhr.send();
 };
-console.log(window.location.protocol);
+
+/*f.watch("./kml-files", { persistent: true }, function (event, filename) {
+  location.reload();
+});*/
+var oldFile=[];
+
+setInterval(function(){
+  getJSON(window.location.protocol + '//'+ window.location.hostname + ':3030',
+  function(err, data) {
+    if (err !== null) {
+      alert('Something went wrong: ' + err);
+    }
+    else {
+      var file_arr = Object.values(data);
+      //console.log(oldFile);
+      //console.log(file_arr);
+
+      var flag = false;
+        if(oldFile.length!==file_arr.length)
+          flag=true;
+        if(oldFile.length === file_arr.length){
+          for(var i=0;i<oldFile.length;i++){
+            if(oldFile[i]!==file_arr[i]){
+              flag=true;
+            }
+          }
+        }
+
+      //console.log(flag);
+      if(flag) {
+        mymap.eachLayer(function (layer) {
+              mymap.remove(layer);
+        });
+        mymap = L.map('map').setView([23.552488, 87.293613],15);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+          maxZoom: 18,
+          id: 'mapbox.streets',
+          accessToken: 'pk.eyJ1IjoiYmhvbGEtYjJjIiwiYSI6ImNqYmdicWlmMDFnM2IycnF3bm9meDlieDIifQ.6edZqKmL3_kl0crsw-UQvw'
+        }).addTo(mymap);
+        for(var i=0;i<file_arr.length;i++) {
+          if(file_arr[i]!=null)
+          {
+            omnivore.kml('./kml-files/'+file_arr[i]).addTo(mymap);
+          }
+        }
+        oldFile=file_arr;
+      }
+    }
+  });
+  },1000);
+
+/*console.log(window.location.protocol);
 console.log(window.location.hostname);
-getJSON(window.location.protocol + '//'+ window.location.hostname + ':3000',
+getJSON(window.location.protocol + '//'+ window.location.hostname + ':3030',
 function(err, data) {
   if (err !== null) {
     alert('Something went wrong: ' + err);
   } else {
     var file_arr = Object.values(data);
     for(var i=0;i<file_arr.length;i++) {
-    	console.log(file_arr[i]);
-    	omnivore.kml('./kml-files/'+file_arr[i]).addTo(mymap);
+      if(file_arr[i]!=null)
+      {
+    	 console.log(file_arr[i]);
+    	 omnivore.kml('./kml-files/'+file_arr[i]).addTo(mymap);
+      }
     }
   }
-});
+});*/
--- fetch-filename.js
+++ fetch-filename.js
@@ -3,6 +3,15 @@ var app = express();
 
 const testFolder = './kml-files/';
 const fs = require('fs-readdir-promise');
+var f = require('fs');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 var fileName = [];
 var j=0;
 
@@ -16,18 +25,25 @@ fs(testFolder)
   		console.log(err.message);
 	});
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+f.watch("./kml-files", { persistent: true }, function (event, filename) {
+	fileName=[];
+  fs(testFolder)
+	.then(function(files) {
+			j=0;
+  		files.forEach(file => {
+    		fileName[j++] = file;
+		})
+  	//console.log(fileName);
+	})
+	.catch(function(err) {
+  		console.log(err.message);
+	});
 });
-
-
 
 app.get('/', function(req,res) {
-	res.send(fileName);
+		res.send(fileName);
 });
 
-app.listen('3000', function() {
-	console.log('Running on port 3000');
+app.listen('3030', function() {
+	console.log('Running on port 3030');
 });
